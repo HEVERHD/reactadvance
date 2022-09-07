9f33c746f456e89ceac18765e486591a2228b434
unknown <hever11.hdgd@gmail.com>
2022-09-07 10:37:38 -0500

    x

--- src/components/Login.js
+++ src/components/Login.js
@@ -17,49 +17,48 @@ export const Login = () => {
 
   const submitHandler = (e) => {
     e.preventDefault();
-    const email = e.target.email.value;
-    const password = e.target.password.value;
+    // const email = e.target.email.value;
+    // const password = e.target.password.value;
 
-    // eslint-disable-next-line
-    const mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    // // eslint-disable-next-line
+    // const mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-    if (email === "" || password === "") {
-      Swal.fire({
-        icon: "error",
-        title: "Oops...",
-        text: "Por favor los campos no deben estar vacios!",
-        footer: '<a href="">Why do I have this issue?</a>',
-      });
-      return;
-    }
+    // if (email === "" || password === "") {
+    //   Swal.fire({
+    //     icon: "error",
+    //     title: "Oops...",
+    //     text: "Por favor los campos no deben estar vacios!",
+    //     footer: '<a href="">Why do I have this issue?</a>',
+    //   });
+    //   return;
+    // }
 
-    if (email !== "" && !mailformat.test(email)) {
-      Swal.fire({
-        icon: "error",
-        title: "Debe ser un correo valido",
-      });
-      return;
-    }
+    // if (email !== "" && !mailformat.test(email)) {
+    //   Swal.fire({
+    //     icon: "error",
+    //     title: "Debe ser un correo valido",
+    //   });
+    //   return;
+    // }
 
-    if (email !== "challenge@alkemy.org" || password !== "react") {
-      Swal.fire({
-        icon: "error",
-        title: "Oops...",
-        text: "Las credenciales deben ser validas!",
-        footer: '<a href="">Revisa nuevamente</a>',
-      });
-      return;
-    }
+    // if (email !== "challenge@alkemy.org" || password !== "react") {
+    //   Swal.fire({
+    //     icon: "error",
+    //     title: "Oops...",
+    //     text: "Las credenciales deben ser validas!",
+    //     footer: '<a href="">Revisa nuevamente</a>',
+    //   });
+    //   return;
+    // }
 
-    axios.post("/api/login", { email, password }).then((res) => {
-      Swal.fire("Datos correctos");
-      const tokenRecibido = res.data.token;
-      sessionStorage.setItem("token", tokenRecibido);
-      navigate("/listado");
-    });
+    // axios.post("/api/login", { email, password }).then((res) => {
+    //   Swal.fire("Datos correctos");
+    //   const tokenRecibido = res.data.token;
+    //   sessionStorage.setItem("token", tokenRecibido);
+    //   navigate("/listado");
+    // });
   };
 
-  // console.log({ email, password })
   const dispatch = useDispatch();
 
   const onGoogleSignIn = () => {
@@ -78,8 +77,7 @@ export const Login = () => {
             className="p-5 m-auto shadow-sm rounded-lg"
           >
             <h1 className="shawow-sm tex-success mt-5 p-3 text-center rounded">
-              {" "}
-              Iniciar session{" "}
+              Inicia sesión
             </h1>
             <form onSubmit={submitHandler}>
               <div className="mb-3">
@@ -106,21 +104,30 @@ export const Login = () => {
                   className="form-control"
                 />
               </div>
-              <div className="d-flex ">
+              <div className="d-grid col-12 mx-auto">
                 <button
                   type="submit"
-                  // onClick={submitHandler}
-                  // type="submit"
-                  className="btn btn-primary d-flex "
+                  className="btn btn-primary d-flex justify-content-center"
                 >
                   Inicia sesión
                 </button>
+
                 <button
                   onClick={onGoogleSignIn}
                   // type="submit"
-                  className="btn btn-primary d-flex "
+                  className="btn btn-primary d-flex my-3 justify-content-center "
                 >
-                  Inicia sesión con Google
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="bi bi-google"
+                    viewBox="0 0 16 16"
+                  >
+                    Google
+                    <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z" />{" "}
+                  </svg>
                 </button>
               </div>
             </form>
